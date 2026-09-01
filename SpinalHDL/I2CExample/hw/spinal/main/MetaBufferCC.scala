f4650861bdbda0c3acbc55fd641c2005907a4c88
Tomek Barabasz <tomek.barabasz@gmail.com>
2026-09-01 13:50:14 +0200

    poprawiny MetaBufferCC (poprawne nazwy sygnałów)

--- SpinalHDL/I2CExample/hw/spinal/main/MetaBufferCC.scala
+++ SpinalHDL/I2CExample/hw/spinal/main/MetaBufferCC.scala
@@ -19,41 +19,65 @@ import spinal.core._
  * (flaga `stalled`) - inaczej losowy `chaos` moglby zatrzymac sygnal na zawsze,
  * co nie ma nic wspolnego z fizyka.
  *
- * @param input sygnal z innej domeny zegarowej
- * @param chaos jeden bit na kazdy bit `input`: 1 = "ten przerzutnik nie zdazyl".
- *              Sterowany z testbencha; w syntezie zwiazalbys go na stale do zera.
+ * Przy `chaos = 0` zachowuje sie identycznie jak zwykly BufferCC o glebokosci 2.
+ *
+ * Struktura celowo odwzorowuje spinal.lib BufferCC: Component z io + object apply,
+ * ktory chowa instalacje. Dzieki granicy modulu sygnaly wewnetrzne maja w VCD
+ * wlasna hierarchie zamiast wpadac do przestrzeni nazw wolajacego.
  */
-object MetaBufferCC {
+class MetaBufferCC(width: Int) extends Component {
 
-  def apply(input: Bits, chaos: Bits): Bits = new Area {
-    require(
-      chaos.getWidth == input.getWidth,
-      "chaos musi miec tyle samo bitow co input - kazdy przerzutnik rozstrzyga sie niezaleznie"
-    )
+  val io = new Bundle {
+    val dataIn  = in Bits (width bits) // sygnal z innej domeny zegarowej
+    val chaos   = in Bits (width bits) // 1 = "ten przerzutnik nie zdazyl sie rozstrzygnac"
+    val dataOut = out Bits (width bits)
+  }
 
-    val result = Bits(input.getWidth bits)
+  // Kazdy bit dostaje wlasna Area -> w VCD wyladuje jako bits_0_stage0,
+  // bits_1_stage0 itd., zamiast anonimowego stage0_1, stage0_2, ...
+  val bits = for (i <- 0 until width) yield new Area {
 
-    for (i <- 0 until input.getWidth) {
-      // pierwszy stopien: zwykle probkowanie sygnalu z obcej domeny
-      val stage0 = RegNext(input(i)) init (False)
-      stage0.addTag(crossClockDomain)
+    // pierwszy stopien: zwykle probkowanie sygnalu z obcej domeny
+    val stage0 = RegNext(io.dataIn(i)) init (False)
+    stage0.addTag(crossClockDomain)
 
-      // drugi stopien z modelowana zwloka
-      val stage1  = Reg(Bool()) init (False)
-      val stalled = Reg(Bool()) init (False)
+    // drugi stopien z modelowana zwloka
+    val stage1  = Reg(Bool()) init (False)
+    val stalled = Reg(Bool()) init (False)
 
-      when(stage1 =/= stage0) {
-        when(chaos(i) && !stalled) {
-          stalled := True // pierwszy stopien jeszcze sie klaruje
-        } otherwise {
-          stage1  := stage0
-          stalled := False
-        }
+    when(stage1 =/= stage0) {
+      when(io.chaos(i) && !stalled) {
+        stalled := True // pierwszy stopien jeszcze sie klaruje
       } otherwise {
+        stage1  := stage0
         stalled := False
       }
-
-      result(i) := stage1
+    } otherwise {
+      stalled := False
     }
-  }.result
+
+    io.dataOut(i) := stage1
+  }
+}
+
+object MetaBufferCC {
+
+  /**
+   * @param input sygnal z innej domeny zegarowej
+   * @param chaos jeden bit na kazdy bit `input`, sterowany z testbencha
+   * @param name  opcjonalna nazwa instancji - warto podac, bo inaczej
+   *              w hierarchii zobaczysz metaBufferCC_1, metaBufferCC_2
+   */
+  def apply(input: Bits, chaos: Bits, name: String = null): Bits = {
+    require(
+      chaos.getWidth == input.getWidth,
+      s"chaos ma ${chaos.getWidth} bitow, input ${input.getWidth} - kazdy przerzutnik rozstrzyga sie niezaleznie"
+    )
+
+    val c = new MetaBufferCC(input.getWidth)
+    if (name != null) c.setName(name)
+    c.io.dataIn := input
+    c.io.chaos  := chaos
+    c.io.dataOut
+  }
 }
